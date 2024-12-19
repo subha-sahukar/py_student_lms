6920b64718054bb909397f14dd8127205eba256e
subha-sahukar <96671950+subha-sahukar@users.noreply.github.com>
2024-12-19 14:28:50 +0530

    Update testchild program 4
    program 4 added

--- testchild.py
+++ testchild.py
@@ -1,2 +1,20 @@
 #test child branch 
 print ('Inside new file created in Child branch')
+
+# Example 4 Replace Values in a List using For Loop
+
+# define list
+l = ['Hardik', 'Rohit', 'Rahul', 'Virat', 'Pant']
+
+for i in range(len(l)):
+
+    # replace hardik with shardul
+    if l[i] == 'Hardik':
+        l[i] = 'Shardul'
+
+    # replace pant with ishan
+    if l[i] == 'Pant':
+        l[i] = 'Ishan'
+
+# print list
+print(l)
