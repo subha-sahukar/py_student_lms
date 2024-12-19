fd6425a83e4c046c93edf35b6f9a3feb0afdb2da
subha-sahukar <96671950+subha-sahukar@users.noreply.github.com>
2024-12-19 14:16:41 +0530

    Update practice_ex1
    add new program

--- practice_ex1.py
+++ practice_ex1.py
@@ -1,4 +1,10 @@
-
+#Practice example to print max of two numbers
 a = 7
 b = 3
 print(max(a, b))
+
+# defining new list 
+l = ['cat', 'dog', 'sheep', 'camel']
+l.[3] = 'goat'
+# print list
+print(l)
